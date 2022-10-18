5c0653de222922feacd90eeaeae8cc3d33c975dd
acse-cd1f515d <jingyang.min22@imperial.ac.uk>
2022-10-18 17:51:23 +0100

    suppress the flake8 error in init.py

--- simple_functions/__init__.py
+++ simple_functions/__init__.py
@@ -1,7 +1,7 @@
 from pkg_resources import DistributionNotFound, get_distribution
 
-from .constants import *
-from .functions1 import *
+from .constants import *  # noqa
+from .functions1 import *  # noqa
 
 try:
     __version__ = get_distribution(__name__).version
