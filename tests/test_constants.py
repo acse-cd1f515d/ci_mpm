2436a2bd07d018ab2c390e206425c641c542896f
acse-cd1f515d <jingyang.min22@imperial.ac.uk>
2022-10-21 00:21:48 +0100

    show python version

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -10,3 +10,7 @@ class TestPi(object):
         '''Test computation of pi'''
         my_pi = pi(2)
         assert np.isclose(my_pi, np.pi, atol=1e-12)
+
+    def test_python_version():
+        from platform import python_version
+        assert python_version() == '3.10.6'
